ea50a15399e569acb08bde2f6d2577a0cc3f9390
gandolh <cristian.gusatu02@e-uvt.ro>
2022-10-27 15:24:34 +0300

    setup made

---
+++ src/App.js
@@ -0,0 +1,14 @@
+// import logo from './logo.svg';
+import './App.css';
+import './navbar/navbar'
+function App() {
+  return (
+    <div className="App">
+      <navbar/>
+      {/* <SearchArea/>
+      <Map/> */}
+    </div>
+  );
+}
+
+export default App;
