2e50ba1ef108068f205bdced8bb8e1796e0b84ee
gandolh <29356778+gandolh@users.noreply.github.com>
2022-11-22 11:07:18 +0200

    optimizare register

--- src/Pages/Register.jsx
+++ src/Pages/Register.jsx
@@ -1,6 +1,7 @@
 import { Box } from '@mui/system';
 import TextField from '@mui/material/TextField';
 import { Button, Typography } from '@mui/material';
+import { useState } from 'react';
 const registerWrapperStyle = {
     display: 'flex',
     justifyContent: 'center',
@@ -11,26 +12,44 @@ const registerBoxStyle = {
     borderRadius: '12px',
     background: '#e0e0e0',
     boxShadow: '7px 7px 19px #d0d0d0, -7px -7px 19px #f0f0f0',
-    display:'grid',
-    gridTemplateColumns:'1fr 1fr',
-    flexDirection:'column',
-    alignItems:'center',
-    p:'20px',
-    boxSizing:'border-box',
-    gap:'0 20px'
+    display: 'grid',
+    gridTemplateColumns: '1fr 1fr',
+    flexDirection: 'column',
+    alignItems: 'center',
+    p: '20px',
+    boxSizing: 'border-box',
+    gap: '0 20px'
 }
 
-const Register = () => {    
+
+
+
+
+const Register = () => {
+    const [registerData, setRegisterData] = useState({
+        'firstName': '',
+        'lastName': '',
+        'email': '',
+        'confirmEmail': '',
+        'password': '',
+        'confirmPassword': ''
+    });
+
+    const handleChangeRegisterData = (e, key) => {
+        console.log(e.target.value, key)
+    }
+
     return (<Box sx={registerWrapperStyle}>
         <Box sx={registerBoxStyle}>
-            <Typography sx={{gridColumn:'1 / 3'}}>Inregistrare</Typography>
-            <TextField sx={{width:'350px'}} variant="standard" margin="normal" label="nume" />
-            <TextField sx={{width:'350px'}} variant="standard" margin="normal" label="prenume" />
-            <TextField sx={{width:'350px'}} variant="standard" margin="normal" label="email" />
-            <TextField sx={{width:'350px'}} variant="standard" margin="normal" label="confirmare email" />
-            <TextField sx={{width:'350px'}} variant="standard" margin="normal" label="parola" />
-            <TextField sx={{width:'350px'}} variant="standard" margin="normal" label="confirmare parola" />
-            <Button variant='outlined' sx={{marginTop: '50px',gridColumn:'1 / 3'}}> Inregistrare</Button>
+            <Typography sx={{ gridColumn: '1 / 3' }}>Inregistrare</Typography>
+            {Object.keys(registerData).map(l =>
+                <TextField
+                    key={l}
+                    sx={{ width: '350px' }}
+                    variant="standard" margin="normal" label={l}
+                    onChange={(e) => handleChangeRegisterData(e, l)} />
+            )}
+            <Button variant='outlined' sx={{ marginTop: '50px', gridColumn: '1 / 3' }}> Inregistrare</Button>
         </Box>
     </Box>);
 }
